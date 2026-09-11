203140549d96c00c7e7018fbd1e0503be493e8b9
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2026-09-11 09:05:06 -0300

    RF-011: register missing Koin use cases across mock-location-setup and routing
    Fixes a crash on launch: ObserveSetupStateUseCase, ComputeRouteUseCase,
    DownloadRegionUseCase and ObserveRegionCatalogUseCase were never bound in
    their presentation modules, so Koin failed to resolve them.

--- feature/mocklocationsetup/presentation/src/main/kotlin/com/routeforge/mocklocationsetup/presentation/di/MockLocationSetupPresentationModule.kt
+++ feature/mocklocationsetup/presentation/src/main/kotlin/com/routeforge/mocklocationsetup/presentation/di/MockLocationSetupPresentationModule.kt
@@ -1,10 +1,13 @@
 package com.routeforge.mocklocationsetup.presentation.di
 
+import com.routeforge.mocklocationsetup.domain.usecase.ObserveSetupStateUseCase
 import com.routeforge.mocklocationsetup.presentation.MockLocationSetupViewModel
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val mockLocationSetupPresentationModule =
     module {
+        factoryOf(::ObserveSetupStateUseCase)
         viewModelOf(::MockLocationSetupViewModel)
     }
--- feature/routing/presentation/src/main/kotlin/com/routeforge/routing/presentation/di/RoutingPresentationModule.kt
+++ feature/routing/presentation/src/main/kotlin/com/routeforge/routing/presentation/di/RoutingPresentationModule.kt
@@ -1,13 +1,20 @@
 package com.routeforge.routing.presentation.di
 
+import com.routeforge.routing.domain.usecase.ComputeRouteUseCase
+import com.routeforge.routing.domain.usecase.DownloadRegionUseCase
+import com.routeforge.routing.domain.usecase.ObserveRegionCatalogUseCase
 import com.routeforge.routing.presentation.RegionCatalogViewModel
 import com.routeforge.routing.presentation.RouteRequestViewModel
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val routingPresentationModule =
     module {
+        factoryOf(::ComputeRouteUseCase)
+        factoryOf(::DownloadRegionUseCase)
+        factoryOf(::ObserveRegionCatalogUseCase)
         viewModel { RouteRequestViewModel(computeRoute = get(), lastComputedRouteHolder = get()) }
         viewModelOf(::RegionCatalogViewModel)
     }
